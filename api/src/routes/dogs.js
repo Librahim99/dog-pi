e39cf7327cb2d780f7c38b9e56c8281e84a86861
Lucas Ibrahim <lucasibrahim@MacBook-Air-de-Agustin.local>
2021-11-23 19:54:35 -0300

    searchbyname

--- api/src/routes/dogs.js
+++ api/src/routes/dogs.js
@@ -1,18 +1,63 @@
-const { Router } = require('express');
-const { Dog } = require('../db')
-
+const { Router, application } = require('express');
+const { Dog, Temperament } = require('../db')
+const axios = require('axios')
+const{API_KEY} = process.env
 
 const server = Router();
 
-server.get('/', (req, res, next)=>{
-    return Dog.findAll()
-    .then((dog)=>{ 
-        res.json(dog)
-    })
-    .catch((error) =>{
+
+server.get('/', async (req, res, next)=>{
+    try{
+        const apiUrl = await axios.get('https://api.thedogapi.com/v1/breeds',{headers:{'x-api-key':`${API_KEY}`}})
+        const dogDb = await Dog.findAll({
+            include: Temperament,
+        })
+        const DogsApi = apiUrl.data
+        const filteredDogsApi = DogsApi.map((d) => {
+            return{
+                id: d.id,
+                name: d.name,
+                height: d.height,
+                weight: d.weight,
+                life_span: d.life_span,
+                temperament:d.temperament
+            }
+        })
+        const allDogs = [...filteredDogsApi, ...dogDb]
+        if(req.query.name){
+            const name = req.query.name
+            const foundDog = allDogs.find((d) => d.name == name)
+            res.send(foundDog)
+            }
+        else res.send(allDogs)
+
+    }
+    catch{(error)=>{
         next(error)
-    })
+    }}
 })
+
+server.get('/search' ,async (req, res, next)=>{
+    const name = req.query.name
+    const apiUrl = await axios.get('https://api.thedogapi.com/v1/breeds',{headers:{'x-api-key':`${API_KEY}`}})
+        const dogDb = await Dog.findAll({
+            include: Temperament,
+        })
+   const foundDog = apiUrl.data.find((d) => d.name == name)
+        res.send(foundDog)
+})
+
+// server.get('/', (req, res, next)=>{
+//     return Dog.findAll({ 
+//         include: Temperament
+//     })
+//     .then((dog)=>{ 
+//         res.json(dog)
+//     })
+//     .catch((error) =>{
+//         next(error)
+//     })
+// })
 
 server.post('/', (req, res, next)=>{
     const {name, height, weight, life_span} = req.body
@@ -22,12 +67,22 @@ server.post('/', (req, res, next)=>{
         weight, 
         life_span}) 
     .then((newDog) =>{
-        newDog
         res.status(201).json(newDog)
     })
     .catch((error) =>{
         next(error)
     })
+})
+
+server.post('/:dogID/temperament/:temperamentID/', (req, res, next) =>{
+    const{dogID, temperamentID} = req.params
+    return Dog.findByPk(dogID)
+    .then((dog) =>{
+        dog.addTemperament(temperamentID)
+    res.send(200)
+}) .catch((error) =>{
+    next(error)
+})
 })
 
 server.delete('/', (req, res, next)=>{
--- api/src/routes/temperaments.js
+++ api/src/routes/temperaments.js
@@ -1,23 +1,45 @@
 const { Router } = require('express');
-const { Temperament } = require('../db')
+const { Temperament, Dog } = require('../db');
+const axios = require('axios')
+const{API_KEY} = process.env
 
 const server = Router();
 
-server.get('/', (req, res, next)=>{
-    return Temperament.findAll()
-    .then((temperament) =>{
-        res.json(temperament)
-    })
-    .catch((error) =>{
-        next(error)
-    })
+
+server.get('/', async (req, res) => {
+    const apiUrl = await axios.get('https://api.thedogapi.com/v1/breeds', {headers: {'x-api-key': `${API_KEY}`}});
+    // const temperamentsDb= await Temperament.findAll()
+    const temperament = apiUrl.data.map(element => element.temperament)
+    let temperaments = temperament.toString().trim().split(/\s*,\s*/);
+    let splittemperament = temperaments.filter(word => word.length > 0);
+    splittemperament.forEach(element => {
+        Temperament.findOrCreate({
+            where: {name: element}
+        })
+    });
+    const allTemperament = await Temperament.findAll();
+    res.send(allTemperament);
 })
+
+
+
+// server.get('/', (req, res, next)=>{
+//     return Temperament.findAll({
+//         include: Dog
+//     }
+//     )
+//     .then((temperament) =>{
+//         res.json(temperament)
+//     })
+//     .catch((error) =>{
+//         next(error)
+//     })
+// })
 
 server.post('/', (req, res, next)=>{
     const{name} = req.body
     return Temperament.create({name})
     .then((newTemperament)=>{
-        newTemperament
         res.json(newTemperament)
     })
     .catch((error) =>{
